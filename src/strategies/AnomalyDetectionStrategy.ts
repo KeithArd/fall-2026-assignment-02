56d71c5dfbd12925e87f9cab2a8b5581d33c4129
Sam Huizar <ahuizar@charlotte.edu>
2026-09-16 14:33:56 -0400

    feat: implement external call, implement scan for outliers, implement scan for flagged status

--- src/strategies/AnomalyDetectionStrategy.ts
+++ src/strategies/AnomalyDetectionStrategy.ts
@@ -1,4 +1,4 @@
-import { Transaction } from '../models.js';
+import { AnomalyRules, Transaction } from '../models.js';
 import { AnomalyRulesService } from '../services/AnomalyRulesService.js';
 import { AuditStrategy } from './AuditStrategy.js';
 
@@ -11,14 +11,35 @@ export class AnomalyDetectionStrategy implements AuditStrategy {
     transactions: Transaction[],
     customParam?: string,
   ): Promise<string> {
-    // TODO: Feature 2 - Implement this strategy.
-    // 1. Call AnomalyRulesService.getRules() asynchronously.
-    // 2. Scan transactions to find outliers (expenses exceeding rules.maxTransactionAmount).
-    // 3. Scan to identify duplicates (transactions sharing the exact same date, category, description, and amount).
-    // 4. Identify transactions having a status that matches any in rules.flaggedStatuses.
-    // 5. Calculate total flagged value and anomaly rates.
-    // 6. Format and return a text-based audit report of anomalies, duplicate sets, and totals.
+    
+    // Calls AnomalyRulesService.getRules() to get the rules
+    const rules = await AnomalyRulesService.getRules();
+      
+    // Scans to find outliers (transactions greater than the max transaction amount)
+    const outliers: Transaction[] = transactions.filter((tx) => tx.amount > rules.maxTransactionAmount);
 
+    // 3. Scan to identify duplicates (transactions sharing the exact same date, category, description, and amount).
+     
+
+    // Scans to find transactions with a flagged status
+    const statusFlagged: Transaction[] = transactions.filter((tx) => rules.flaggedStatuses.includes(tx.status));
+
+    // 5. Calculate total flagged value and anomaly rates.
+    
+    // 6. Format and return a text-based audit report of anomalies, duplicate sets, and totals.
+    const textReport = '============\n' +
+    ' Report of Anomalies and Duplicates\n' +
+    '--------------\n' +
+    'Outlier Transactions: ' + //add amt outliers
+    '--------------\n' +
+    'Dplicate Transactions: ' + //add dupes
+    '--------------\n' +
+    'Flagged Transactions: ' + //add flagged
+    '--------------\n' +
+    'Total Anomalous Transactions: ' + //add total amt
+    'Percentage of Anomalous Transactions: '; //add percents
+
+    return textReport;
     throw new Error('Method not implemented.');
   }
 }
